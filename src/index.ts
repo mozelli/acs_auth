c99fa85ebad4b4d7935fc98fee0571bb3041cc6e
João Mozelli Neto <joaomozelli@hotmail.com>
2023-12-28 12:22:22 -0300

    activate cors

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,10 @@ import { routes } from './routes';
 dotenv.config();
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+// const birth = new Date("1982-04-21");
+// console.log(birth);
+
 app.listen(process.env.SERVER_PORT, () => console.log(`Server is running. PORT: ${process.env.SERVER_PORT}`));
