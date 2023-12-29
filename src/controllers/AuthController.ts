f22ca7f39067e40862f0ee6352ac7af4f3475d8c
João Mozelli Neto <joaomozelli@hotmail.com>
2023-12-29 08:04:36 -0300

    include claims and access token into response

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,20 +1,11 @@
 import { Request, Response } from "express";
 import { prismaClient } from "../database/prismaClient";
 import bcrypt from 'bcrypt';
+import { accessToken } from "../functions/tokenGenerate";
 
 export class AuthController {
     async authenticate(request: Request, response: Response) {
         const { email, password } = request.body;
-
-        
-
-        // const user = await prismaClient.user.create({
-        //     data: {
-        //         name,
-        //         email,
-        //         password_hash
-        //     }
-        // })
 
         try {
             const auth = await prismaClient.user.findUnique({
@@ -28,13 +19,24 @@ export class AuthController {
                 const password_authenticated = bcrypt.compareSync(password, hash)
 
                 if(password_authenticated) {
-                    return response.status(200).json({message: "Authenticated!"})
+                    return response.status(200).json(
+                        {
+                            message: "Authenticated!", 
+                            user: {
+                                user_id: auth.user_id,
+                                name: auth.name,
+                                email: auth.email,
+                                birthday: auth.birthday,
+                                gender: auth.gender
+                            },
+                            token: accessToken(auth.user_id)
+                        });
                 } else {
-                    return response.status(404).json({message: "Cannot authenticate!"})
+                    return response.status(400).json({message: "Invalid informations."})
                 }
 
             } else {
-                return response.status(404).json({message: "user not found."})
+                return response.status(400).json({message: "User not found."}).header;
             }
         } catch(error) {
             return response.status(500).json({message: "Error in AuthController / authenticate", error});
